f59cb6658614e3f62a4f83f3b422e16909c483cc
sofiakarb <karvoun@di.uoa.gr>
2019-03-13 16:28:41 +0200

    fix parameters

--- files/root/exareme/start-worker.sh
+++ files/root/exareme/start-worker.sh
@@ -6,16 +6,16 @@ mkdir -p /tmp/demo/db
 source /etc/profile
 EXAREME_ADMIN_JMX_PORT=10000
         EXAREME_ADMIN_CLASS_PATH="$EXAREME_HOME/lib/exareme/*:$EXAREME_HOME/lib/exareme/external/*"
-        EXAREME_ADMIN_MASTER_CLASS="madgik.exareme.master.admin.StartMaster"
         EXAREME_ADMIN_WORKER_CLASS="madgik.exareme.worker.admin.StartWorker"
         EXAREME_ADMIN_OPTS="${EXAREME_JAVA_OPTS}  \
                 -Djava.rmi.server.codebase=file:$EXAREME_HOME/lib/exareme/                      \
-                            -Djava.security.policy=$EXAREME_HOME/etc/exareme/art.policy         \
-                            -Djava.rmi.server.hostname=$MY_IP                      \
-                            -Dcom.sun.management.jmxremote.port=$EXAREME_ADMIN_JMX_PORT         \
-                            -Dcom.sun.management.jmxremote.authenticate=false                   \
-                            -Dcom.sun.management.jmxremote.ssl=false                            \
-                            -Djava.security.egd=file:///dev/urandom "
+                -Djava.security.policy=$EXAREME_HOME/etc/exareme/art.policy         \
+                -Djava.rmi.server.hostname=$MY_IP                      \
+                -Dcom.sun.management.jmxremote.port=$EXAREME_ADMIN_JMX_PORT         \
+                -Dcom.sun.management.jmxremote.authenticate=false                   \
+                -Dcom.sun.management.jmxremote.ssl=false                            \
+                -Djava.security.egd=file:///dev/urandom "
+
 DESC="exareme-worker"
             EXAREME_ADMIN_CLASS=$EXAREME_ADMIN_WORKER_CLASS
             EXAREME_ADMIN_CLASS_ARGS=$MASTER_IP
