65eae8473bbcfcf8301d7481ec88f26e5d9eb76b
Thanasis <tkarabatsis@hotmail.com>
2019-03-13 22:11:06 +0200

    Check datasets fix

--- files/root/exareme/set-local-datasets.sh
+++ files/root/exareme/set-local-datasets.sh
@@ -4,11 +4,10 @@ if [ -z ${NODE_NAME} ]; then echo "NODE_NAME is unset";exit;  fi
 if [ -z ${CONSULURL} ]; then echo "CONSULURL is unset"; exit; fi
 
 MADIS="/root/exareme/lib/madis/src/mterm.py"
-DATASETS_PATH="datasets"
-LOCAL_DATASET="/root/mip-algorithms/input_tbl.csv"
+LOCAL_DATASET="/root/exareme/datasets/datasets.csv"
 
 DATASETS=$(echo "select distinct __val from (file header:t file:$LOCAL_DATASET) where __colname = 'dataset';" | $MADIS | \
 	 sed '1d ; $d' | jq .[]  | sed 's/^\"//g ; s/\"$//g' | printf %s "$(cat)"| jq -R -c -s 'split("\n")')
 
 
-curl -X PUT -d @- $CONSULURL/v1/kv/$DATASETS_PATH/$NODE_NAME <<< $DATASETS
+curl -X PUT -d @- $CONSULURL/v1/kv/datasets/$NODE_NAME <<< $DATASETS
